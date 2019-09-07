fcddbf184d336cf243c851448454e2e5513aa53e
Baris <sarikaya.baris@web.de>
2019-09-07 13:15:14 +0200

    Update

--- TestProject/src/StudentGui.java
+++ TestProject/src/StudentGui.java
@@ -1,6 +1,8 @@
 import javafx.application.Application;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class StudentGui extends Application {
@@ -10,12 +12,18 @@ public class StudentGui extends Application {
 		
 		BorderPane bp = new BorderPane();
 		
-		
+		bp.setTop(TopOfTestCenter());
 		Scene scene = new Scene(bp,900,600);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("TestCenter");
 		primaryStage.show();
 
+	}
+
+	private Node TopOfTestCenter() {
+		HBox hbox =new HBox();
+		
+		return null;
 	}
 
 }
