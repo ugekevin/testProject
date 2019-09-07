e1aba36bfeab804f1314a27a1e369dde84654751
ugekevin <kevinihrig@web.de>
2019-09-07 13:27:50 +0200

    prof und admin selber stand wie student

--- TestProject/src/ProfGui.java
+++ TestProject/src/ProfGui.java
@@ -1,12 +1,37 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class ProfGui extends Application {
 
 	@Override
-	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
+	public void start(Stage primaryStage) throws Exception {
+
+	BorderPane bp = new BorderPane();
+		
+		bp.setTop(TopOfTestCenter());
+		Scene scene = new Scene(bp,900,600);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("TestCenter");
+		primaryStage.show();
 
 	}
 
+	private Node TopOfTestCenter() {
+		HBox hbox =new HBox();
+		Text t = new Text("Testcenter");
+		hbox.setAlignment(Pos.CENTER);
+		t.setFont( Font.font( "Calibri", FontWeight.NORMAL, 30 ) );
+		
+		hbox.getChildren().addAll(t);
+		return hbox;
+	}
 }
