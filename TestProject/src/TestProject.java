463f4d97b0388d5fb9de11064ed020f3c63223df
Baris <sarikaya.baris@web.de>
2019-09-07 12:12:05 +0200

    HI

--- TestProject/src/TestProject.java
+++ TestProject/src/TestProject.java
@@ -4,9 +4,9 @@ public class TestProject {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("hallo baris");
 		System.out.println("was geht abaaabbabababa");
 		System.out.println("noob");
+		System.out.println("noe");
 	}
 
 }
