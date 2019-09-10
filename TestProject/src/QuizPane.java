c91c7abdc21f3b80d877e62ade3573721494a379
Baris <sarikaya.baris@web.de>
2019-09-10 12:07:20 +0200

    quizpane

---
+++ TestProject/src/QuizPane.java
@@ -0,0 +1,14 @@
+
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class QuizPane extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
