7d1fabaee2c328ea2b16ffab38cb10f13cdd4fe2
ugekevin <kevinihrig@web.de>
2019-09-10 11:38:05 +0200

    main änderung

--- TestProject/src/TestProject.java
+++ TestProject/src/TestProject.java
@@ -5,16 +5,9 @@ public class TestProject {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-	//	Application.launch(ProjectGui.class, args);
+	Application.launch(ProjectGui.class, args);
 
-		//Application.launch(StudentGui.class, args);
-	//Application.launch(ProfGui.class, args);
-
-	//	Application.launch(StudentGui.class, args);
-	//	Application.launch(ProfGui.class, args);
-
-		Application.launch(AdminGui.class, args);
-		System.out.println("Hi");
+	
 	}
 
 }
